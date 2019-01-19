fe93c313d9f54ecce55edca23903fd2efde78fe8
Nick Holbrook <ndh175@gmail.com>
2019-01-19 05:36:13 -0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PredictiveTaxiHeatmap
+//
+//  Created by Nick Holbrook on 1/19/19.
+//  Copyright © 2019 Nick Holbrook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
